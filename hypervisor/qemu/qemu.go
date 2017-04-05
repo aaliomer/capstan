6b3dc621234da8913fb74a7a1852d43fc2288292
Aliomer, Ayad <ayad.aliomer@optum.com>
2017-04-05 09:24:38 -0400

    added mac osx and windows support

--- hypervisor/qemu/qemu.go
+++ hypervisor/qemu/qemu.go
@@ -256,8 +256,11 @@ func (c *VMConfig) vmArguments(version *Version) ([]string, error) {
 	args = append(args, "-m", strconv.FormatInt(c.Memory, 10))
 	args = append(args, "-smp", strconv.Itoa(c.Cpus))
 	args = append(args, "-device", "virtio-blk-pci,id=blk0,bootindex=0,drive=hd0")
-	args = append(args, "-drive", "file="+c.Image+",if=none,id=hd0,cache="+c.vmDriveCache())
-	//args = append(args, "-drive", "file="+c.Image+",if=none,id=hd0,aio=native,cache="+c.vmDriveCache())
+	if runtime.GOOS != "linux" {
+		args = append(args, "-drive", "file="+c.Image+",if=none,id=hd0,cache="+c.vmDriveCache())
+	} else {
+		args = append(args, "-drive", "file="+c.Image+",if=none,id=hd0,aio=native,cache="+c.vmDriveCache())
+	}
 	if version.Major >= 1 && version.Minor >= 3 {
 		args = append(args, "-device", "virtio-rng-pci")
 	}
